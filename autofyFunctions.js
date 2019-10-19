5171f00862809ae08242f126f54c1493a7021e29
VimbaMikus <56762699+VimbaMikus@users.noreply.github.com>
2019-10-19 22:07:45 +0300

    Added front-end files

---
+++ autofyFunctions.js
@@ -0,0 +1,21 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsiveNav() {
+    var navbar = document.getElementById("topNav");
+    if (navbar.className === "nav_bar") {
+      navbar.className += " responsive";
+    } else {
+      navbar.className = "nav_bar";
+    }
+  }
+ window.onscroll = function() {myFunction()};
+  // Get the offset position of the navbar
+  var navbar = document.getElementById("topNav");
+  var sticky = navbar.offsetTop;
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
